973927c0cc43dc989b8aefa0d180ebab2a4a4122
stefanobazzi <stefanobazzi@yahoo.it>
2014-12-10 15:44:48 +0100

    basic server

--- sserver.py
+++ sserver.py
@@ -1,9 +1,27 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-app = Flask(__name__)
+#! /usr/bin/env python
+# -*- unicode:utf-8 -*-
 
-@app.route("/hello")
+from flask import Flask, request, Response, render_template, flash, redirect, url_for
+from functools import wraps
+import os.path
+
+app = Flask(__name__)
+app.secret_key = 'pippo'
+user_dir = os.path.dirname(os.path.realpath(__file__))
+users = {}
+
+def check_user(user, pwd):
+	return USER == user and PWD == pwd
+
+
+class User(object):
+
+	def __init__(self, name=None, pwd=None):
+		self.name = name
+		self.pwd = pwd
+
+user = User()
+
 def hello():
 	return "hello"
 
